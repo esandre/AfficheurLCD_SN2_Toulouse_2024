b0c801383d1584d544f23c7bc8fdb469b8c35fc6
Enzo Sandre <enzo@enzosandre.fr>
2024-03-05 09:52:38 +0100

    Partie 2 du prototype

--- AfficheurLCD/Program.cs
+++ AfficheurLCD/Program.cs
@@ -1,32 +1,53 @@
-﻿void Print_1()
+﻿using System.Text;
+
+string Print1()
 {
-    Console.WriteLine("  " + Environment.NewLine +
-                      "  |" + Environment.NewLine +
-                      "  |");
+    return "   " + Environment.NewLine +
+           "  |" + Environment.NewLine +
+           "  |";
 }
 
-void Print_2()
+string Print2()
 {
-    Console.WriteLine(" _ " + Environment.NewLine +
-                      " _|" + Environment.NewLine +
-                      "|_ ");
+    return  " _ " + Environment.NewLine +
+            " _|" + Environment.NewLine +
+            "|_ ";
 }
 
-void Print(uint nb)
+string Concat(string a, string b)
+{
+    var linesA = a.Split(Environment.NewLine).GetEnumerator();
+    using var disposeA = linesA as IDisposable;
+
+    var linesB = b.Split(Environment.NewLine).GetEnumerator();
+    using var disposeB = linesB as IDisposable;
+
+    var builder = new StringBuilder();
+    while (linesA.MoveNext() && linesB.MoveNext())
+    {
+        builder.Append(linesA.Current);
+        builder.Append(' ');
+        builder.Append(linesB.Current);
+        builder.AppendLine();
+    }
+
+    return builder.ToString();
+}
+
+string Print(uint nb)
 {
     switch (nb)
     {
         case 1:
-            Print_1();
-            break;
+            return Print1();
         case 2:
-            Print_2();
-            break;
+            return Print2();
         default:
-            Print(nb / 10);
-            Print(nb % 10);
-            break;
+            return Concat(
+                Print(nb / 10),
+                Print(nb % 10)
+            );
     }
 }
 
-Print(12111);
+Console.WriteLine(Print(12111));
