c994647c63a3fd71fe5d53c19ccd4424bb96722b
Enzo Sandre <enzo@enzosandre.fr>
2024-03-05 09:24:08 +0100

    Ajoutez des fichiers projet.

---
+++ AfficheurLCD/Program.cs
@@ -0,0 +1,32 @@
+﻿void Print_1()
+{
+    Console.WriteLine("  " + Environment.NewLine +
+                      "  |" + Environment.NewLine +
+                      "  |");
+}
+
+void Print_2()
+{
+    Console.WriteLine(" _ " + Environment.NewLine +
+                      " _|" + Environment.NewLine +
+                      "|_ ");
+}
+
+void Print(uint nb)
+{
+    switch (nb)
+    {
+        case 1:
+            Print_1();
+            break;
+        case 2:
+            Print_2();
+            break;
+        default:
+            Print(nb / 10);
+            Print(nb % 10);
+            break;
+    }
+}
+
+Print(12111);
